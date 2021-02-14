483827b892658eb7631e05848dd5f481a72246dd
Paulo Alexandre Regis <regisin@gmail.com>
2021-02-14 10:03:19 -0600

    Hello website

--- build.py
+++ build.py
@@ -1,1 +1,18 @@
+from jinja2 import Environment, FileSystemLoader
+from pathlib import Path
 
+
+""" Serve the website
+python -m http.server --directory ./_site/
+"""
+
+file_loader = FileSystemLoader('templates')
+env = Environment(loader=file_loader)
+
+template = env.get_template('hello.html')
+output = template.render(name="Harry Potter")
+o = Path.cwd()
+o = o / Path('_site/index.html')
+with o.open(mode='w') as fh:
+    fh.write(output)
+    
--- setup.sh
+++
@@ -1,6 +0,0 @@
-mkdir simple_static_website
-cd simple_static_website
-echo "" > build.py
-echo "" > data.py
-mkdir templates
-mkdir _site
