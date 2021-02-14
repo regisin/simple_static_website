342e638c04c378415d9376140632177dc7c3c0c8
Paulo Alexandre Regis <regisin@gmail.com>
2021-02-14 10:14:10 -0600

    Adding pages

--- build.py
+++ build.py
@@ -9,10 +9,38 @@ python -m http.server --directory ./_site/
 file_loader = FileSystemLoader('templates')
 env = Environment(loader=file_loader)
 
-template = env.get_template('hello.html')
-output = template.render(name="Harry Potter")
+template = env.get_template('base.html')
+output = template.render(
+  title="INDEX"
+)
 o = Path.cwd()
 o = o / Path('_site/index.html')
+with o.open(mode='w') as fh:
+    fh.write(output)
+    
+"""
+About
+"""
+# /about.html
+template = env.get_template('about.html')
+output = template.render(
+  title="ABOUT ME"
+)
+o = Path.cwd()
+o = o / Path('_site/about.html')
+with o.open(mode='w') as fh:
+    fh.write(output)
+
+"""
+Teaching
+"""
+# /teaching.html
+template = env.get_template('teaching.html')
+output = template.render(
+  title="TEACHING"
+)
+o = Path.cwd()
+o = o / Path('_site/teaching.html')
 with o.open(mode='w') as fh:
     fh.write(output)
     
